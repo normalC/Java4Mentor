cee9d2980734ffe5f98e8e606753ee6d3a9bb97d
normalC <990031126@qq.com>
2019-10-21 13:56:38 +0800

    creatproj

--- JavaIntegration/OOBoM/OOBoM/runit-new.bat
+++
@@ -1,7 +0,0 @@
-C:\Program Files\Java\jdk1.6.0_04\bin\java.exe -cp .;
-C:\WDIR\OTAKI-AATK\Training\Java Integration\jip_EXP2005_win32\jacob.jar;
-C:\WDIR\OTAKI-AATK\Training\Java Integration\jip_EXP2005_win32\jip_ExpeditionPCB.jar;
-C:\WDIR\OTAKI-AATK\Training\Java Integration\jip_EXP2005_win32\jip_MGCPCBAutomationLicensing.jar;
-"C:/Program Files/OpenOffice.org 2.0/program/classes/ridl.jar";
-"C:/Program Files/OpenOffice.org 2.0/program/classes/unoil.jar";
-"C:/Program Files/OpenOffice.org 2.0/program/classes/juh.jar" OOBoM
